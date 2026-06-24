92186861607dd12f2151154e2fe69a294573da13
dhiksn <andhikarafi321@gmail.com>
2026-06-24 22:42:29 +0700

    feat: new project

--- assets/js/script.js
+++ assets/js/script.js
@@ -303,7 +303,7 @@ function renderProjects() {
           <h3 class="project-title">${p.title}</h3>
           <p class="project-desc">${p.description}</p>
           <div class="project-btns">
-            <a href="project/dokumentasi-asat-tahun-2026" class="proj-btn proj-primary">
+            <a href="${p.readUrl || 'project/dokumentasi-asat-tahun-2026'}" class="proj-btn proj-primary">
               Baca
               <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
             </a>
